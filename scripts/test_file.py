1b0bceb27e20fc9986298e29f9619253907f3a56
hitheima <hitheima@163.com>
2018-08-05 09:17:29 +0800

    first commit

---
+++ scripts/test_file.py
@@ -0,0 +1,11 @@
+class TestFile:
+
+    def test_file1(self):
+        assert 1
+
+    def test_file2(self):
+        assert 1
+
+    def test_file3(self):
+        assert 0
+
---
+++ scripts/test_scripts.py
@@ -0,0 +1,12 @@
+import pytest
+
+
+class TestScripts:
+
+    def test_scripts1(self):
+        assert 1
+
+    def test_scripts2(self):
+        assert 1
+
+
